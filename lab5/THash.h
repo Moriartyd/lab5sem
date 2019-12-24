f1f716e7b89bd4b27fa2dd124bc9aa9e71dc56e9
Moriartyd <dima@galeev.ru>
2019-12-25 02:20:55 +0300

    Без рехэша

---
+++ lab5/THash.h
@@ -0,0 +1,215 @@
+#pragma once
+#include <string>
+#include <iostream>
+#define DELIMITER 10
+
+using namespace std;
+
+int	ft_atoi(char c)
+{
+	if (c >= '0' && c <= '9')
+		return (c);
+	return (0);
+}
+
+template <typename TValue1, typename TValue2, typename TKey>
+class THash
+{
+	template <typename Value1, typename Value2, typename Key>
+	struct Owner
+	{
+		Owner(Value1 n=0, Value2 m=0, Key nu=0, Owner* nex=0) :name(n), model(m), num(nu), next(nex) {}
+
+		Value1 name;
+		Value2 model;
+		Key	   num;
+		Owner* next;
+	};
+
+	int						HashFunc(TKey key)
+	{
+		int hash;
+		unsigned i = 0;
+
+		hash = 0;
+		while (i < key.length())
+		{
+			hash += ft_atoi(key[i]);
+			i++;
+		}
+		hash = hash % DELIMITER;
+		return (hash);
+	}
+	int cnt = 10;
+	int size = 0;
+	Owner<TValue1, TValue2, TKey>** table;
+public:
+	THash();
+	void push(TValue1 n, TValue2 m, TKey nu);
+	void show();
+	void find(TKey key);
+	void del(TKey key);
+	void show_ind();
+	~THash();
+};
+
+template<typename TValue1, typename TValue2, typename TKey>
+inline THash<TValue1, TValue2, TKey>::THash()
+{
+	if (!(table = new Owner<TValue1, TValue2, TKey> * [cnt]))
+		exit(-1);
+	int i = 0;
+	while (i < cnt)
+	{
+		table[i] = 0;
+		i++;
+	}
+}
+
+template<typename TValue1, typename TValue2, typename TKey>
+inline void THash<TValue1, TValue2, TKey>::push(TValue1 n, TValue2 m, TKey nu)
+{
+	int index = HashFunc(nu);
+	Owner<TValue1, TValue2, TKey>* el = new Owner<TValue1, TValue2, TKey>(n, m, nu);
+	size++;
+	if (!table[index])
+	{
+		table[index] = el;
+		table[index]->next = 0;
+		return;
+	}
+	Owner<TValue1, TValue2, TKey>* tmp = table[index];
+	while (tmp->next)
+		tmp = tmp->next;
+	tmp->next = el;
+	el->next = 0;
+}
+
+template<typename TValue1, typename TValue2, typename TKey>
+inline void THash<TValue1, TValue2, TKey>::show()
+{
+	Owner<TValue1, TValue2, TKey>* tmp;
+	int i = 0;
+	int j;
+	while (i < cnt)
+	{
+		if (table[i])
+		{
+			j = 0;
+			cout << "Элемент №" << i << ":" << endl;
+			tmp = table[i];
+			while (tmp)
+			{
+				if (j > 0)
+					cout << "Элемент №" << i <<"/" << j << ":" << endl;
+				cout << "\tИмя: " << tmp->name << endl;
+				cout << "\tМарка: " << tmp->model << endl;
+				cout << "\tНомер: " << tmp->num << endl;
+				tmp = tmp->next;
+				j++;
+			}
+		}
+		i++;
+	}
+}
+
+template<typename TValue1, typename TValue2, typename TKey>
+inline void THash<TValue1, TValue2, TKey>::find(TKey key)
+{
+	Owner<TValue1, TValue2, TKey>*	tmp;
+	int								index;
+
+	index = HashFunc(key);
+	tmp = table[index];
+	if (!tmp)
+		cout << "Элемент не найден :(" << endl;
+	else
+	{
+		while (tmp && tmp->num != key)
+			tmp = tmp->next;
+		if (tmp)
+		{
+			cout << "Владелец автомобиля:" << endl;
+			cout << "\tИмя: " << tmp->name << endl;
+			cout << "\tМарка: " << tmp->model << endl;
+			cout << "\tНомер: " << tmp->num << endl;
+		}
+		else
+			cout << "Элемент не найден :(" << endl;
+	}
+}
+
+template<typename TValue1, typename TValue2, typename TKey>
+inline void THash<TValue1, TValue2, TKey>::del(TKey key)
+{
+	Owner<TValue1, TValue2, TKey>* tmp;
+	Owner<TValue1, TValue2, TKey>* prev;
+
+	int ind;
+
+	ind = HashFunc(key);
+	tmp = table[ind];
+	prev = 0;
+	while (tmp && tmp->num != key)
+	{
+		prev = tmp;
+		tmp = tmp->next;
+		size--;
+	}
+	if (tmp && tmp == table[ind])
+	{
+		table[ind] = table[ind]->next;
+		delete tmp;
+		size--;
+	}
+	else if (tmp && !tmp->next)
+	{
+		prev ? prev->next = 0 : 0;
+		delete tmp;
+		size--;
+	}
+	else if (tmp)
+	{
+		prev ? prev = tmp->next : 0;
+		delete tmp;
+	}
+	else
+		cout << "Элемент не найден :(" << endl;
+	tmp = 0;
+}
+
+template<typename TValue1, typename TValue2, typename TKey>
+inline void THash<TValue1, TValue2, TKey>::show_ind()
+{
+	Owner<TValue1, TValue2, TKey>* tmp;
+	int i;
+	int j;
+
+	i = 0;
+	while (i < cnt)
+	{
+		if (table[i])
+		{
+			cout << i;
+			j = 0;
+			tmp = table[i];
+			while (tmp)
+			{
+				j++;
+				tmp = tmp->next;
+			}
+			if (j > 1)
+				cout << "/" << j << "\t";
+		}
+		i++;
+	}
+	cout << endl;
+}
+
+template<typename TValue1, typename TValue2, typename TKey>
+inline THash<TValue1, TValue2, TKey>::~THash()
+{
+	for (int i = 0; i < cnt; i++)
+		delete[] table[i];
+	delete[] table;
+}
---
+++ lab5/lab5.cpp
@@ -0,0 +1,73 @@
+﻿#include <iostream>
+#include "THash.h"
+
+void    show_menu()
+{
+    cout << "====================================МЕНЮ====================================" << endl;
+    cout << " 1. Вставить элемент" << endl
+        << " 2. Вывод таблицы" << endl
+        << " 3. Список индексов" << endl
+        << " 4. Найти элемент по ключу" << endl
+        << " 5. Удалить элемент по ключу" << endl
+        << " 0. Выход" << endl;
+    cout << "============================================================================" << endl;
+    cout << "Ваш выбор: ";
+}
+
+void    insert(THash<string, string, string>*& hashTable)
+{
+    string name, num, model;
+
+    cout << "Введите имя: "; cin >> name;
+    cout << "Введите модель: "; cin >> model;
+    cout << "Введите номер: "; cin >> num;
+    hashTable->push(name, model, num);
+}
+
+void    find_element(THash<string, string, string>*& hashTable)
+{
+    string key;
+    cout << "Введите ключ: "; cin >> key;
+    hashTable->find(key);
+}
+
+void    del_element(THash<string, string, string>*& hashTable)
+{
+    string key;
+    cout << "Введите ключ: "; cin >> key;
+    hashTable->del(key);
+}
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+    THash<string, string, string>* hashTable = new THash<string, string, string>();
+    int menu = 1;
+
+    while (menu) {
+        show_menu();
+        cin >> menu;
+        switch (menu)
+        {
+        case 1:
+            insert(hashTable);
+            break;
+        case 2:
+            hashTable->show();
+            break;
+        case 3:
+            hashTable->show_ind();
+            break;
+        case 4:
+            find_element(hashTable);
+            break;
+        case 5:
+            del_element(hashTable);
+            break;
+        default:
+            exit(0);
+            break;
+        }
+    }
+    return (0);
+}
